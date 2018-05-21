623333b49cf46d74d4a374642fd91c62f7f32ba6
AllTwinkleStars <KeepGrowinga@gmail.com>
2018-05-21 16:28:32 -0600

    App Cleanup

--- src/app.js
+++ src/app.js
@@ -1,125 +1,40 @@
-const express = require("express");
+const fs = require('fs');
 const path = require("path");
+const express = require("express");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-let savingsTransactions = [
-  {
-    date: "05/14/2018",
-    type: "Credit",
-    description: "SHARP*EMPLOYER",
-    status: "cleared",
-    amount: "$2100.00"
-  },
-  {
-    date: "05/13/2018",
-    type: "Debit",
-    description: "WITHDRAWL",
-    status: "cleared",
-    amount: "$600.00"
-  }
-]
+let accountData = fs.readFileSync(path.join(__dirname, "json", "accounts.json"))
+let accounts = JSON.parse(accountData);
 
-let checkingTransactions = [
-  {
-    date: "05/14/2018",
-    type: "Debit",
-    description: "PS INST XFER",
-    status: "cleared",
-    amount: "$10.00"
-  },
-  {
-    date: "05/13/2018",
-    type: "Debit",
-    description: "MICRO PAY",
-    status: "cleared",
-    amount: "$41.74"
-  },
-  {
-    date: "05/13/2018",
-    type: "Debit",
-    description: "CAFE PS",
-    status: "cleared",
-    amount: "$6.76"
-  },
-  {
-    date: "05/13/2018",
-    type: "Debit",
-    description: "LOCAL*GROCER",
-    status: "cleared",
-    amount: "$145.32"
-  }
-] 
+let userData = fs.readFileSync(path.join(__dirname, "json", "users.json"))
+let users = JSON.parse(userData);
 
-let cardTransactions = [
-  {
-    date: "05/14/2018",
-    type: "Debit",
-    description: "ACME GAMES",
-    status: "cleared",
-    amount: "$193.25"
-  },
-  {
-    date: "05/13/2018",
-    type: "Debit",
-    description: "YO*BOOKSTORE",
-    status: "cleared",
-    amount: "$134.23"
-  },
-  {
-    date: "05/13/2018",
-    type: "Debit",
-    description: "The PS Store",
-    status: "cleared",
-    amount: "$54.67"
-  }
-]
+let savings = accounts.savings;
+let checking = accounts.checking;
+let credit = accounts.credit;
 
+app.get("/", (req, res) => res.render("index", { title: "Index", account: accounts }));
+app.get("/savings", (req, res) => res.render("account", { title: "Savings", account: savings }));
+app.get("/checking", (req, res) => res.render("account", { title: "Checking", account: checking }));
+app.get("/credit", (req, res) => res.render("account", { title: "Credit Card", account: credit }));
+app.get("/profile", (req, res) => res.render("profile", { title: "Profile", user: users[0] }));
 
-app.get("/", (req, res) => res.render("index", { 
-  title: "Index",
-  savings: savingsTransactions,
-  checking: checkingTransactions,
-  card: cardTransactions
- }));
-
-app.get("/savings", (req, res) => {
-  res.render("transactions", {
-    title: "Savings",
-    transactions: savingsTransactions
-  });
-});
-
-app.get("/checking", (req, res) => {
-  res.render("transactions", {
-    title: "Checking",
-    transactions: checkingTransactions
-  });
-});
-
-app.get("/cards", (req, res) => {
-  res.render("transactions", {
-    title: "Cards",
-    transactions: cardTransactions
-  });
-});
-
-
-app.get("/profile", (req, res) => {
-  res.render("profile", {
-    title: "Profile",
-    details: {
-      name: "PS User",
-      username: "psuser",
-      phone: "801-555-0101",
-      email: "psuser@example.com",
-      address: "1212 PS Circle, Project, Learning, 55555"
-    }
-  });
+app.get("/transfer", (req, res) =>  res.render("transfer", { title: "Transfer", msg: req.query.msg }));
+app.post('/transfer', (req, res) => {
+  let fromNew = accounts[req.body.from].balance - req.body.amount;
+  let toNew = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+  accounts[req.body.from].balance = fromNew;
+  accounts[req.body.to].balance = toNew;
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), JSON.stringify(accounts), 'utf8');
+  let string = encodeURIComponent("Transfer Completed");
+  res.redirect('/transfer?msg=' + string);
 });
 
 app.listen("3000", () => console.log("PS Project Running on port 3000!"));
