5ae2a0d0409263e3037da3cb9c4ef3beda965197
AllTwinkleStars <KeepGrowinga@gmail.com>
2018-06-01 12:43:03 -0600

    Remove the need for title.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // 1.1
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
 
 // 1.2
@@ -41,64 +41,60 @@ app.get("/", (req, res) => res.render("index", { title: "Accounts Summary", acco
 // 2.5 in views/index.ejs
 
 // 2.6
-app.get("/savings", (req, res) => res.render("account", { title: savings.nickname, account: savings }));
+app.get("/savings", (req, res) => res.render("account", { account: savings }));
 
 // 2.7
-app.get("/checking", (req, res) => res.render("account", { title: checking.nickname, account: checking }));
+app.get("/checking", (req, res) => res.render("account", { account: checking }));
 
 // 2.8
-app.get("/credit", (req, res) => res.render("account", { title: credit.nickname, account: credit }));
+app.get("/credit", (req, res) => res.render("account", { account: credit }));
 
 // 2.9 in views/account.ejs
 
 // 2.10
-app.get("/profile", (req, res) => res.render("profile", { title: "Profile", user: users[0] }));
+app.get("/profile", (req, res) => res.render("profile", { user: users[0] }));
 
 // 2.11 in views/profile.ejs
 
 
 // 3.2
-app.get("/transfer", (req, res) =>  res.render("transfer", { title: "Transfer", msg: req.query.msg }));
+app.get("/transfer", (req, res) => res.render("transfer"));
 
 // 3.3 in views/transfer.ejs
 
 // 3.4
-app.post('/transfer', (req, res) => {
+app.post("/transfer", (req, res) => {
   // 3.5
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
 
   // 3.6
   let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, 'utf8');
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "utf8");
   
   // 3.7
-  let string = encodeURIComponent("Transfer Completed");
-  res.redirect('/transfer?msg=' + string);
+  res.render("transfer", { message: "Transfer Completed" });
 });
 
 // 4.1 in views/payment.ejs
 
 // 4.2
-app.get("/payment", (req, res) =>  res.render("payment", { title: "Make a Payment", account: credit, msg: req.query.msg }));
+app.get("/payment", (req, res) =>  res.render("payment", { account: credit }));
 
 // 4.3
-app.post('/payment', (req, res) => {
+app.post("/payment", (req, res) => {
   // 4.4
-  accounts['credit'].balance -= req.body.amount;
-  accounts['credit'].available = parseInt(accounts['credit'].available) + parseInt(req.body.amount);
+  accounts["credit"].balance = accounts["credit"].balance - req.body.amount;
+  accounts["credit"].available = parseInt(accounts["credit"].available) + parseInt(req.body.amount);
 
   // 4.5
   let accountsJSON = JSON.stringify(accounts, null, 4)
 
   // 4.6
-  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, 'utf8');
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "utf8");
   
-  // 4.7
-  let string = encodeURIComponent("Payment Successful");
-
-  // 4.8
-  res.redirect('/payment?msg=' + string);
+  //4.7
+  res.render("payment", { account: credit, message: "Payment Successful" });
 });
 
 
