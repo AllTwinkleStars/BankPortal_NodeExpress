8e80cf2e19914a28c0d47d9bcd355f0a3af1bb1f
AllTwinkleStars <KeepGrowinga@gmail.com>
2018-06-07 13:11:15 -0600

    Globals for test

--- src/tests/globals.js
+++ src/tests/globals.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
 const request = require('supertest');
+const app = require('../app.js');
 
-Object.assign(global, { assert: chai.assert, request });
+Object.assign(global, { assert: chai.assert, request, app });
